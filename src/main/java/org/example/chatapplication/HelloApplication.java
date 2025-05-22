4ddfffc4185a1606cdc842ecb91d6c9ac3003d5a
Lahiru_Mudith <lahimudith@gmail.com>
2025-05-23 02:53:03 +0530

    done project but have bug

--- src/main/java/org/example/chatapplication/HelloApplication.java
+++ src/main/java/org/example/chatapplication/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         startServer();
-        startClient();
+//        startClient();
     }
     void startServer() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/server.fxml"));
@@ -22,7 +22,7 @@ public class HelloApplication extends Application {
         stage.show();
     }
     void startClient() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/client.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/view/client.fxml"));
         Stage stage = new Stage();
         Scene scene = new Scene(fxmlLoader.load(), 618.0, 442);
         stage.setTitle("Client 01!");
